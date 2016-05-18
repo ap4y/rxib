9bd79dcad1d209f0c9fd7b5fc696594d096d796e
ap4y <lod@pisem.net>
2016-05-18 22:42:44 +1200

    Add ColorHelpers.update_attributes

--- lib/rxib/color_helpers.rb
+++ lib/rxib/color_helpers.rb
@@ -5,10 +5,19 @@ module RXib
     def self.parse_to_attributes(el, value)
       color = ColorConversion::Color.new(value)
       r, g, b = color.rgb.values
-      el.attribute(:red, default: format_color(r))
-      el.attribute(:green, default: format_color(g))
-      el.attribute(:blue, default: format_color(b))
-      el.attribute(:alpha, default: color.alpha.to_s)
+      el.property(:red, default: format_color(r))
+      el.property(:green, default: format_color(g))
+      el.property(:blue, default: format_color(b))
+      el.property(:alpha, default: color.alpha.to_s)
+    end
+
+    def self.update_attributes(el, value)
+      color = ColorConversion::Color.new(value)
+      r, g, b = color.rgb.values
+      el.set(:red, format_color(r))
+      el.set(:green, format_color(g))
+      el.set(:blue, format_color(b))
+      el.set(:alpha, color.alpha.to_s)
     end
 
     private
